3a3896aaae54477e53f1ff639751889dc6fcb3ed
jtimko16 <jantimko16@gmail.com>
2025-02-09 20:23:30 +0100

    Mod - prepare production notebook based mostly on functions

--- functions/fun_model.py
+++ functions/fun_model.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 def recommend_books(selected_ISBN: str, df: pd.DataFrame) -> pd.DataFrame:
@@ -70,3 +71,66 @@ def not_recommended_books(selected_ISBN: str, df: pd.DataFrame) -> pd.DataFrame:
     book_counts = book_counts.sort_values(by="Count_not_recommend", ascending=False)
 
     return book_counts
+
+
+def prepare_data_assoc_rules(
+    df: pd.DataFrame,
+    threshold_recomm: int = 7,
+    n_books: int = 1000,
+    min_rating_per_user: int = 25,
+    verbose: int = 0,
+) -> pd.DataFrame:
+    ### Create a new binary category for rating (everything above 8 is 1, otherwise 0)
+    df["Recommend"] = np.where(df["Book-Rating"] >= threshold_recomm, 1, 0)
+    df["Not_Recommend"] = np.where(
+        (df["Book-Rating"] < threshold_recomm) & (df["Book-Rating"] > 0), 1, 0
+    )
+
+    top_n_books = (
+        df.groupby("ISBN").size().sort_values(ascending=False).head(n_books).index
+    )
+
+    # Filter the DataFrame to include only the top N books
+    subset_df_books = df[df["ISBN"].isin(top_n_books)].copy()
+
+    # Filter the DataFrame to include only users with at least N ratings
+    user_counts = df.groupby("User-ID").size()
+    users_with_at_least_n_ratings = user_counts[
+        user_counts >= min_rating_per_user
+    ].index
+    subset_df_book_users = subset_df_books[
+        subset_df_books["User-ID"].isin(users_with_at_least_n_ratings)
+    ]
+
+    if verbose == 1:
+        print(f"Unique User-IDs: {len(subset_df_book_users['User-ID'].unique())}")
+        print(f"Unique ISBNs: {len(subset_df_book_users['ISBN'].unique())}")
+
+    return subset_df_book_users
+
+
+def rank_books(
+    df,
+    count_col="Count_Recommend",
+    percent_col="Percent_Recommend",
+    top_n=10,
+    w1=0.5,
+    w2=0.5,
+) -> pd.DataFrame:
+    """Ranks books based on Count_Recommend and % Recommendation."""
+
+    # Normalize values between 0 and 1
+    df[count_col + "_norm"] = (df[count_col] - df[count_col].min()) / (
+        df[count_col].max() - df[count_col].min()
+    )
+    df[percent_col + "_norm"] = (df[percent_col] - df[percent_col].min()) / (
+        df[percent_col].max() - df[percent_col].min()
+    )
+
+    # Compute score
+    df["Score"] = w1 * df[count_col + "_norm"] + w2 * df[percent_col + "_norm"]
+
+    # Select top books
+    top_books = df.sort_values(by="Score", ascending=False).head(top_n)
+
+    return top_books[["Book-Title", "Score", count_col, percent_col]]
